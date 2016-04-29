56231a6a5106c325fa7238c031e8f53cbf95739a
monni94 <monni_3000@yahoo.com>
2016-04-29 21:50:09 +0300

    calc x*5

--- app/src/main/java/com/example/menna/calculator/MainActivity.java
+++ app/src/main/java/com/example/menna/calculator/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.menna.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button ;
+    TextView textView ;
+    EditText editText ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button) ;
+        textView = (TextView) findViewById(R.id.textView2);
+        editText = (EditText)findViewById(R.id.editText) ;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer res = Integer.parseInt(editText.getText().toString())*5;
+                textView.setText(res.toString());
+            }
+        });
     }
 }
